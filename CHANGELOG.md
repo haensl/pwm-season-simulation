41dc0883643145bd3a650c9031822be1bcad6ead
HP Dietz <hp@jandh.org>
2017-04-23 00:35:26 +0200

    Fix luminosity calculation leftover bug.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
+## 1.0.2
+* Fix luminosity calculation leftover bug
+
+## 1.0.1
+* Fix compiler warnings
+
 ## 1.0.0
 * Initial project
--- moonlight.ino
+++ moonlight.ino
@@ -7,7 +7,6 @@ const byte PIN_MOONLIGHT = 6;
 const byte LUMINOSITY_MAX = 16;
 const byte LUMINOSITY_INCREMENT = 1;
 const unsigned long MILLIS_PER_LUMINOSITY_STEP = (12L * 60L * 1000L);
-const unsigned int BAUT_CONSOLE = 9600;
 const unsigned long BAUT_BRIDGE = 115200L;
 const unsigned int BOOT_DELAY = 5000;
 
@@ -16,6 +15,8 @@ const byte HOURS_START = 21;
 const byte HOURS_END = (HOURS_START + HOURS_OF_MOONLIGHT) % 24;
 
 byte LUMINOSITY_MODIFIER = 1;
+byte luminosity;
+unsigned long millisAtLastIteration;
 
 void setup() {
   Console.begin();
@@ -25,26 +26,26 @@ void setup() {
   Console.println("Bridging to AR9331.");
   Bridge.begin(BAUT_BRIDGE);
   pinMode(PIN_MOONLIGHT, OUTPUT);
+  luminosity = 0;
+  millisAtLastIteration = 0;
   Console.println("Starting moonlight simulation");
 }
 
 void loop() {
   byte luminosity = 0;
-  if (moonlightEnabled()) {
+  byte currentHour = getHours();
+
+  if (currentHour >= HOURS_START || currentHour <= HOURS_END) {
     luminosity = getLuminosity();
+  } else {
+    luminosity = 0;
+    millisAtLastIteration = 0;
   }
-  
+
   analogWrite(PIN_MOONLIGHT, luminosity);
 }
 
-boolean moonlightEnabled() {
-  byte currentHour = getHours();
-  return currentHour >= HOURS_START || currentHour <= HOURS_END;
-}
-
 byte getLuminosity() {
-  static byte luminosity = 0;
-  static unsigned long millisAtLastIteration = 0;
   unsigned long millisNow = millis();
 
   if (millisNow - millisAtLastIteration >= MILLIS_PER_LUMINOSITY_STEP) {
