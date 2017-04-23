86fadd0447efe9311e7c85bb485f5688f627fa70
HP Dietz <hp@jandh.org>
2017-04-23 17:17:20 +0200

    Refactor. Extract magic numbers.

--- moonlight.ino
+++ moonlight.ino
@@ -9,11 +9,25 @@ typedef struct Time {
 } Time;
 
 const byte PIN_MOONLIGHT = 6;
-const byte LUMINOSITY_MAX = 16; // maximum luminosity -- i.e. pwm at full moon
+const byte LUMINOSITY_MAX_MOONLIGHT = 16; // maximum luminosity -- i.e. pwm at full moon
+const byte MAX_MOONLIGHT_HOURS_PER_DAY = 12;
+const byte DAYS_PER_MONTH = 30;
+const byte HOURS_PER_DAY = 24;
 
-const unsigned long MILLIS_PER_LUMINOSITY_STEP = (12L * 60L * 1000L);
+/**
+ * Skews adjust the moonlight luminosity based on location around the globe.
+ */
+const float SKEW_START_OF_YEAR_MOONLIGHT_LUMINOSITY = 4.2f;
+const float SKEW_MONTHLY_MOONLIGHT_LUMINOSITY = 4.3f;
+
+/**
+ * Set correct baut rate depending on board.
+ */
 const unsigned long BAUT_BRIDGE = 115200L;
-const unsigned int BAUT_SERIAL = 9600;
+
+/**
+ * Wait before initializing the bridge, to give the linux kernel time to boot.
+ */
 const unsigned int BOOT_DELAY = 5000;
 
 boolean mooncycleCompleted;
@@ -29,6 +43,10 @@ void setup() {
 }
 
 void loop() {
+  analogWrite(PIN_MOONLIGHT, moonlight());
+}
+
+byte moonlight() {
   static unsigned int dayOfYear;
   static byte dayOfMonth;
   static byte hoursOfMoonlight;
@@ -45,21 +63,20 @@ void loop() {
 
   byte moonlightLuminosity = getMoonlightLuminosity(hoursOfMoonlight, hourOfMoonrise, maximumMoonlightLuminosityOfDay, getTime());
   mooncycleCompleted = moonlightShiningAtLastIteration && moonlightLuminosity <= 0;
-
-  analogWrite(PIN_MOONLIGHT, moonlightLuminosity);
   moonlightShiningAtLastIteration = moonlightLuminosity > 0;
+  return moonlightLuminosity;
 }
 
 byte getHoursOfMoonlight(unsigned int dayOfYear) {
-  return 6 * (2 / M_PI) * asin(cos(M_PI * dayOfYear / 30 + 4.2)) + 6;
+  return (byte)round((MAX_MOONLIGHT_HOURS_PER_DAY / M_PI) * asin(cos(M_PI * dayOfYear / DAYS_PER_MONTH + SKEW_START_OF_YEAR_MOONLIGHT_LUMINOSITY)) + (MAX_MOONLIGHT_HOURS_PER_DAY / 2.0f));
 }
 
 byte getHourOfMoonrise(unsigned int dayOfYear) {
-  return 12 * (2 / M_PI) * asin(cos(M_PI * dayOfYear / 30 + 4.3)) + 12;
+  return (byte)round((HOURS_PER_DAY / M_PI) * asin(cos(M_PI * dayOfYear / DAYS_PER_MONTH + SKEW_MONTHLY_MOONLIGHT_LUMINOSITY)) + (HOURS_PER_DAY / 2.0f));
 }
 
 byte getMaximumMoonlightLuminosity(byte dayOfMonth) {
-  return -(LUMINOSITY_MAX / 2) * (2 / M_PI) * asin(cos(M_PI * dayOfMonth / 15)) + (LUMINOSITY_MAX / 2);
+  return (byte)round(-(LUMINOSITY_MAX_MOONLIGHT / 2.0f) * (2.0f / M_PI) * asin(cos(M_PI * dayOfMonth / (DAYS_PER_MONTH / 2.0f))) + (LUMINOSITY_MAX_MOONLIGHT / 2.0f));
 }
 
 byte getMoonlightLuminosity(byte hoursOfMoonlight, byte hourOfMoonrise, byte maxLuminosity, Time t) {
@@ -75,7 +92,6 @@ byte getMoonlightLuminosity(byte hoursOfMoonlight, byte hourOfMoonrise, byte max
 
 Time getTime() {
   Time t;
-
   Process date;
   date.begin("/bin/date");
   date.addParameter("+%T");
